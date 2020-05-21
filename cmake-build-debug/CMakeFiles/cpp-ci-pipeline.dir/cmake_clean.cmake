9a4c674c584672bf18e673ee8d89f4b8a0547d12
Jose Solano <joseandressolanomora@gmail.com>
2020-05-21 01:11:16 -0600

    Basic structure

---
+++ cmake-build-debug/CMakeFiles/cpp-ci-pipeline.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-ci-pipeline.dir/main.cpp.o"
+  "cpp-ci-pipeline.pdb"
+  "cpp-ci-pipeline"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-ci-pipeline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by jose on 21/05/20.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout<<"Hello world"<<endl;
+}
