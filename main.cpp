19327a67f210b174f54a303f04f073d005a12d0e
Jose Solano <joseandressolanomora@gmail.com>
2020-05-21 01:31:59 -0600

    Calculator ready

--- main.cpp
+++ main.cpp
@@ -5,6 +5,36 @@
 
 using namespace std;
 
+class Calculator {
+
+public:
+
+    Calculator(){};
+
+    void sumar(int a, int b){
+        cout<<a<<"+"<<b<<" = "<<a+b<<endl;
+    }
+
+    void restar(int a, int b){
+        cout<<a<<"-"<<b<<" = "<<a-b<<endl;
+    }
+
+    void multiplicar(int a, int b){
+        cout<<a<<"*"<<b<<" = "<<a*b<<endl;
+    }
+
+    void dividir(int a, int b){
+        cout<<a<<"/"<<b<<" = "<<a/b<<endl;
+    }
+
+};
+
+
 int main() {
-    cout<<"Hello world"<<endl;
+    cout<<"*****Calculator*****"<<endl;
+    Calculator *calculator;
+    calculator->sumar(2,4);
+    calculator->restar(6,4);
+    calculator->multiplicar(2,4);
+    calculator->dividir(16,4);
 }
